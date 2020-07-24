a5e765ec0b4990b92c08ba49a7f87089fc93f4b6
Stijn <stijn@kolkies.dev>
2020-07-24 22:38:36 +0200

    Add ready event and guild rename on name update

--- src/modules/EventModule.ts
+++ src/modules/EventModule.ts
@@ -1,17 +1,23 @@
 import { Message } from "discord.js";
 import { command, listener, Module, ErisClient } from "@lib/utils";
+import { Guild } from "discord.js";
 
 export default class EventModule extends Module {
   constructor(client: ErisClient) {
-      super(client);
+    super(client);
   }
-  @listener({ event: "message" })
-  onMessage(msg: Message) {
-      console.log("anotherModule onMessage", msg.content);
+  @listener({ event: "guildUpdate" })
+  onGuidUpdate(oldGuild: Guild, newGuild: Guild) {
+    if (newGuild.id == process.env.MAIN_GUILD_ID && newGuild.name !== process.env.MAIN_GUILD_NAME) newGuild.edit({ name: process.env.MAIN_GUILD_NAME });
+  }
+
+  @listener({ event: "ready" })
+  onReady() {
+    console.log("Bot up and running!");
   }
 
   @command()
   ping(msg: Message) {
-      msg.reply("pong");
+    msg.reply("pong");
   }
 }
