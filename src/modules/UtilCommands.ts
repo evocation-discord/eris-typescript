d20667e66d7140bb3f022a165827eb0da44ac77c
Stijn <stijn@kolkies.dev>
2020-07-25 18:09:37 +0200

    Add setgame command

--- src/modules/UtilCommands.ts
+++ src/modules/UtilCommands.ts
@@ -5,9 +5,9 @@ import { PresenceStatusData } from "discord.js";
 export default class UtilCommandModule extends Module {
 
   @command({ inhibitors: [inhibitors.botAdminsOnly], args: [new Remainder(String)] })
-  echo(msg: Message, ...args: string[]) {
+  echo(msg: Message, args: string) {
     msg.delete();
-    msg.channel.send(args.join(" "), { allowedMentions: { parse: [], users: [], roles: [] } });
+    msg.channel.send(args, { allowedMentions: { parse: [], users: [], roles: [] } });
   }
 
   @command({ inhibitors: [inhibitors.botAdminsOnly], args: [String] })
@@ -34,9 +34,27 @@ export default class UtilCommandModule extends Module {
         break;
 
       default:
-        return msg.channel.send("**ERROR**: Status needs to be `online`, `dnd`, `idle` or `invisible`");
+        return msg.channel.send("**ERROR**: Status needs to be `online`, `dnd`, `idle` or `invisible`.");
 
     }
     return msg.channel.send(`**SUCCESS**: My status is now **${status}**.`);
+  }
+
+  @command({ inhibitors: [inhibitors.botAdminsOnly], args: [String, new Remainder(String)] })
+  setgame(msg: Message, type: "watching" | "playing" | "listening", game: string) {
+    switch (type) {
+      case "listening":
+        this.client.user.setActivity(game, { type: "LISTENING" });
+        break;
+      case "watching":
+        this.client.user.setActivity(game, { type: "WATCHING" });
+        break;
+      case "playing":
+        this.client.user.setActivity(game, { type: "PLAYING" });
+        break;
+      default:
+        return msg.channel.send("**ERROR**: Type needs to be `watching`, `playing` or `listening`.");
+    }
+    return msg.channel.send(`**SUCCESS**: I'm now ${type}${type === "listening" ? " to" : ""} **${game}**.`);
   }
 }
