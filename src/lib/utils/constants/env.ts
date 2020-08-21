167beaeaeba81dd094de2aa4750be156f4780427
Stijn <stijn@kolkies.dev>
2020-08-21 18:35:23 +0200

    Add env constants

---
+++ src/lib/utils/constants/env.ts
@@ -0,0 +1,10 @@
+export const ROLES = {
+
+};
+
+export const NEGOTATIONS = {
+  EXPERIENCE: process.env.NEGATION_EXPERIENCE,
+  MEDIA: process.env.NEGATION_MEDIA,
+  REACTIONS: process.env.NEGATION_REACTIONS,
+  ART: process.env.NEGATION_ART,
+};
