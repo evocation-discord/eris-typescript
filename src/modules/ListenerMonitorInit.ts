4e0ce3533edc1df82f6c73903e50aa09d7ed75b0
Stijn <stijn@kolkies.dev>
2020-08-21 18:36:22 +0200

    Update monitorSetup

--- src/modules/ListenerMonitorInit.ts
+++ src/modules/ListenerMonitorInit.ts
@@ -9,25 +9,21 @@ export default class ListenerMonitorInit extends Module {
   @listener({ event: "message" })
   message(...args: unknown[]): void {
     setupMonitor(this.client.monitorManager.monitors, "message", ...args);
-    // Array.from(this.client.monitorManager.monitors).filter(m => m.events.includes("message")).forEach(monitor => monitor.func.call(monitor.module, ...args));
   }
 
   @listener({ event: "messageUpdate" })
   messageUpdate(...args: unknown[]): void {
     setupMonitor(this.client.monitorManager.monitors, "messageUpdate", ...args);
-    // Array.from(this.client.monitorManager.monitors).filter(m => m.events.includes("messageUpdate")).forEach(monitor => monitor.func.call(monitor.module, ...args));
   }
 
   @listener({ event: "messageDelete" })
   messageDelete(...args: unknown[]): void {
     setupMonitor(this.client.monitorManager.monitors, "messageDelete", ...args);
-    // Array.from(this.client.monitorManager.monitors).filter(m => m.events.includes("messageDelete")).forEach(monitor => monitor.func.call(monitor.module, ...args));
   }
 
   @listener({ event: "messageReactionAdd" })
   messageReactionAdd(...args: unknown[]): void {
     setupMonitor(this.client.monitorManager.monitors, "messageReactionAdd", ...args);
-    // Array.from(this.client.monitorManager.monitors).filter(m => m.events.includes("messageReactionAdd")).forEach(monitor => monitor.func.call(monitor.module, ...args));
   }
 }
 
