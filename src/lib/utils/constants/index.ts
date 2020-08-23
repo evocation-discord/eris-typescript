b0528bd7edc621a6398d33c9fed78c45bfe76ce1
Stijn <stijn@kolkies.dev>
2020-08-23 14:25:05 +0200

    linkshortner logger

--- src/lib/utils/constants/index.ts
+++ src/lib/utils/constants/index.ts
@@ -1,3 +1,5 @@
 export * from "./colors";
 export * from "./emotes";
 export * from "./env";
+
+export const linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
--- src/modules/LoggingModule.ts
+++ src/modules/LoggingModule.ts
@@ -1,7 +1,7 @@
-import { Module, listener, monitor, escapeRegex, emotes, CHANNELS } from "@lib/utils";
-import { Message } from "discord.js";
-import { TextChannel } from "discord.js";
-import dayjs from "dayjs";
+import { Module, monitor, escapeRegex, emotes, CHANNELS, linkRegex, ROLES } from "@lib/utils";
+import { Message, TextChannel } from "discord.js";
+import tall from "tall";
+import { linkResolver } from "@lib/utils/linkResolver/linkResolver";
 
 export default class LoggingModule extends Module {
   @monitor({ events: ["message"] })
@@ -25,6 +25,26 @@ export default class LoggingModule extends Module {
     if (cmd.staff || cmd.admin) return channel.send(`\`[${timeFormatter()}]\` **\`[ADMINISTRATIVE]\`** ${msg.client.emojis.resolve(emotes.LOGGING.ADMINISTRATIVE_AUDIT)} **\`${msg.author.tag}\`** (\`${msg.author.id}\`) performed \`${cmdTrigger}\` (\`${msg.id}\`)${stringArgs.length > 0 ? ` with args: \`${stringArgs.join(" ")}\`` : ""} in ${msg.channel} (\`${msg.channel.id}\`).`);
     return channel.send(`\`[${timeFormatter()}]\` ${msg.client.emojis.resolve(emotes.LOGGING.AUDIT)} **\`${msg.author.tag}\`** (\`${msg.author.id}\`) performed \`${cmdTrigger}\` (\`${msg.id}\`)${stringArgs.length > 0 ? ` with args: \`${stringArgs.join(" ")}\`` : ""} in ${msg.channel} (\`${msg.channel.id}\`).`);
   }
+
+  @monitor({ events: ["message"] })
+  async onLink(msg: Message): Promise<Message> {
+    if (msg.author && msg.author.bot) return;
+    if (isStaff(msg)) return;
+    const links = msg.content.match(linkRegex) || [];
+    const channel = await this.client.channels.fetch(CHANNELS.MODERATION_LOG) as TextChannel;
+    if (links.length) {
+      for await (const _link of links) {
+        const link = await linkResolver(_link);
+        if (link === _link) continue;
+        channel.send(
+          `\`[${timeFormatter()}]\` **\`[LINK REDIRECT RESOLVER]\`** ${msg.client.emojis.resolve(emotes.LOGGING.LINK_RESOLVER)} **\`${msg.author.tag}\`** (\`${msg.author.id}\`) sent a message (\`${msg.id}\`) containing a redirection-based link in ${msg.channel} (\`${msg.channel.id}\`).\n\n` +
+          `**UNRESOLVED LINK**: <${_link}>\n` +
+          `**RESOLVED LINK**: <${link}>\n\n` +
+          "No automatic action has been taken against their account or the message itself. Please review the above to ensure that the link is not violative of Evocation's regulations..");
+      }
+    }
+
+  }
 }
 
 const timeFormatter = () => {
@@ -41,3 +61,6 @@ const timeFormatter = () => {
   const month = _month.slice(-2);
   return `${hour}:${minutes} ${day}/${month}/${year} UTC`;
 };
+
+
+const isStaff = (msg: Message): boolean => msg.member.roles.cache.some(role => [ROLES.MODERATION, ROLES.ADMINISTRATORS].includes(role.id));
