bbcdb2efb05da6a93de4d874009f6a71b3bd19b4
Stijn <stijn@kolkies.dev>
2020-08-26 20:26:15 +0200

    Stupid mistake

--- src/lib/utils/constants/messages.ts
+++ src/lib/utils/constants/messages.ts
@@ -2,5 +2,5 @@ import { Message } from "discord.js";
 import { emotes } from "./emotes";
 
 export const RESPONSES = {
-  SUCCESS: (msg: Message, text: string): string => `${msg.client.emojis.resolve(emotes.UNCATEGORISED.SUCCESS)} **SUCCESS**: ${msg}`,
+  SUCCESS: (msg: Message, text: string): string => `${msg.client.emojis.resolve(emotes.UNCATEGORISED.SUCCESS)} **SUCCESS**: ${text}`,
 };
