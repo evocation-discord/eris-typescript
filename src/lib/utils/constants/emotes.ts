ab8e44c8af62197089d8af0883ee6b0521067147
Stijn <stijn@kolkies.dev>
2020-09-13 17:07:35 +0200

    Updated emotes

--- src/lib/utils/constants/emotes.ts
+++ src/lib/utils/constants/emotes.ts
@@ -1,87 +1,93 @@
 const giveaway = {
-  "giftmessage": "<:giftmessage:737019540550189170>",
-  "giftreaction": "<:giftreaction:737019540495663276>",
-  "giftreactionid": "737019540495663276",
-  "donation": "<:donation:748230750805164032>"
+  giftmessage: "<:giftmessage:737019540550189170>",
+  giftreaction: "<:giftreaction:737019540495663276>",
+  giftreactionid: "737019540495663276",
+  donation: "<:donation:752268602068959252>"
 };
 
 const logging = {
-  "anonymisedaudit": "<:anonymisedaudit:750260814879129601>",
-  "administrativeaudit": "<:administrativeaudit:737291912389853457>",
-  "audit": "<:audit:737289964420726806>",
-  "linkresolver": "<:linkresolver:747001085490626650>",
-  "lockchannel": "<:lockchannel:737019540201799801>",
-  "lockdown": "<:lockdown:737278085644288060>",
-  "maintenance": "<:maintenance:746989380710826076>",
-  "messagecreation": "<:messagecreation:737208433672978544>",
-  "messagedeletion": "<:messagedeletion:737019540373766224>",
-  "messageedit": "<:messageedit:737207518727503912>",
-  "nameupdate": "<:nameupdate:747771679488606359>",
-  "unlockchannel": "<:unlockchannel:737019540478623804>",
-  "channelarchived": "<:channelarchived:749193007139520552>"
+  anonymisedaudit: "<:anonymisedaudit:752268602924597269>",
+  administrativeaudit: "<:administrativeaudit:737291912389853457>",
+  audit: "<:audit:737289964420726806>",
+  linkresolver: "<:linkresolver:747001085490626650>",
+  lockchannel: "<:lockchannel:737019540201799801>",
+  lockdown: "<:lockdown:737278085644288060>",
+  maintenance: "<:maintenance:746989380710826076>",
+  messagecreation: "<:messagecreation:737208433672978544>",
+  messagedeletion: "<:messagedeletion:737019540373766224>",
+  messageedit: "<:messageedit:737207518727503912>",
+  nameupdate: "<:nameupdate:747771679488606359>",
+  unlockchannel: "<:unlockchannel:737019540478623804>",
+  channelarchived: "<:channelarchived:752268602354434160>"
 };
 
 const relics = {
-  "absolution": "<:absolution:737022894420262992>",
-  "aceofhearts": "<:aceofhearts:737019541175009330>",
-  "heartbleed": "<:heartbleed:737043659156881510>",
-  "keytorevelation": "<:keytorevelation:744466470595199067>",
-  "libertyofsoul": "<:libertyofsoul:745306759874347089>",
-  "nothingeverchanges": "<:nothingeverchanges:746691496438398997>",
-  "predominance": "<:predominance:737019794435342351>",
-  "ringofreprieve": "<:ringofreprieve:737582865704943728>",
-  "valiant": "<:valiant:739769645581467659>"
+  absolution: "<:absolution:737022894420262992>",
+  aceofhearts: "<:aceofhearts:737019541175009330>",
+  heartbleed: "<:heartbleed:737043659156881510>",
+  keytorevelation: "<:keytorevelation:744466470595199067>",
+  libertyofsoul: "<:libertyofsoul:745306759874347089>",
+  nothingeverchanges: "<:nothingeverchanges:746691496438398997>",
+  predominance: "<:predominance:737019794435342351>",
+  ringofreprieve: "<:ringofreprieve:737582865704943728>",
+  valiant: "<:valiant:739769645581467659>"
 };
 
 const memories = {
-  "memoryi": "<:memoryi:748171804647882873>",
-  "memoryii": "<:memoryii:748171802647331007>",
-  "memoryiii": "<:memoryiii:748171803423146065>",
-  "memoryiv": "<:memoryiv:748171803653832744>",
-  "memoryv": "<:memoryv:748171803653963937>",
-  "memoryvi": "<:memoryvi:748171803980988496>",
-  "memoryvii": "<:memoryvii:748171803284865026>",
-  "memoryviii": "<:memoryviii:748171803901296761>",
-  "memoryix": "<:memoryix:748171803700232222>",
-  "memoryx": "<:memoryx:748171802827817041>",
-  "memoryxi": "<:memoryxi:748174439161790525>",
-  "memoryxii": "<:memoryxii:748171805738532874>",
-  "memoryxiii": "<:memoryxiii:748171806002905178>",
-  "memoryxiv": "<:memoryxiv:748174362212958338>",
-  "memoryxv": "<:memoryxv:748171805801316402>"
+  memoryi: "<:memoryi:748171804647882873>",
+  memoryii: "<:memoryii:748171802647331007>",
+  memoryiii: "<:memoryiii:748171803423146065>",
+  memoryiv: "<:memoryiv:748171803653832744>",
+  memoryv: "<:memoryv:748171803653963937>",
+  memoryvi: "<:memoryvi:748171803980988496>",
+  memoryvii: "<:memoryvii:748171803284865026>",
+  memoryviii: "<:memoryviii:748171803901296761>",
+  memoryix: "<:memoryix:748171803700232222>",
+  memoryx: "<:memoryx:748171802827817041>",
+  memoryxi: "<:memoryxi:748174439161790525>",
+  memoryxii: "<:memoryxii:748171805738532874>",
+  memoryxiii: "<:memoryxiii:748171806002905178>",
+  memoryxiv: "<:memoryxiv:748174362212958338>",
+  memoryxv: "<:memoryxv:748171805801316402>"
 };
 
 const commandresponses = {
-  "admin": "<:admin:747497421399130220>",
-  "denial": "<:denial:747497421327695902>",
-  "endorphin": "<:endorphin:746406018388328499>",
-  "information": "<:information:747497420954534050>",
-  "settings": "<:settings:747497421457588236>",
-  "success": "<:success:747743173501124618>",
-  "server": "<:server:749562562227732572>"
+  admin: "<:admin:747497421399130220>",
+  denial: "<:denial:747497421327695902>",
+  endorphin: "<:endorphin:746406018388328499>",
+  information: "<:information:747497420954534050>",
+  settings: "<:settings:747497421457588236>",
+  success: "<:success:747743173501124618>",
+  server: "<:server:752268602669006909>"
 };
 
 const experiencecapsules = {
-  "tierthreexpcapsule": "<:tierthreexpcapsule:748118154076749876>",
-  "tiertwoxpcapsule": "<:tiertwoxpcapsule:747823453167550494>",
-  "tieronexpcapsule": "<:tieronexpcapsule:747816777139880047>"
+  tierthreexpcapsule: "<:tierthreexpcapsule:748118154076749876>",
+  tiertwoxpcapsule: "<:tiertwoxpcapsule:747823453167550494>",
+  tieronexpcapsule: "<:tieronexpcapsule:747816777139880047>"
 };
 
 const uncategorised = {
-  "enter": "<:enter:746813302113435770>",
-  "fantasyregulations": "<:fantasyregulations:739758249213165598>",
-  "leave": "<:leave:746817814459056208>",
-  "regulations": "<:regulations:739751088932257842>",
-  "creditcard": "<:creditcard:749642425454166078>",
-  "voicechannel": "<:voicechannel:749706276006658260>",
-  "privatevoicechannel": "<:privatevoicechannel:749706275780296866>",
-  "textchannel": "<:textchannel:749706275788685353>",
-  "privatetextchannel": "<:privatetextchannel:749706275935223848>",
-  "announcementchannel": "<:announcementchannel:749706275859726336>",
-  "privateannouncementchannel": "<:privateannouncementchannel:749706275864182865>",
-  "expandedcategory": "<:expandedcategory:749711722130178089>",
-  "soulstones": "<:soulstones:748553442880127097>",
-  "moderation": "<:moderation:752218175986335835>"
+  enter: "<:enter:746813302113435770>",
+  fantasyregulations: "<:fantasyregulations:739758249213165598>",
+  leave: "<:leave:746817814459056208>",
+  regulations: "<:regulations:739751088932257842>",
+  creditcard: "<:creditcard:752268602710949898>",
+  voicechannel: "<:voicechannel:752268602366885901>",
+  privatevoicechannel: "<:privatevoicechannel:752268602534789201>",
+  textchannel: "<:textchannel:752268602698367097>",
+  privatetextchannel: "<:privatetextchannel:752268602698236005>",
+  announcementchannel: "<:announcementchannel:752268602647904306>",
+  privateannouncementchannel: "<:privateannouncementchannel:752268602694041680>",
+  expandedcategory: "<:expandedcategory:752268602404503573>",
+  soulstones: "<:soulstones:752268601880215644>",
+  moderation: "<:moderation:752268602799030282>",
+  donotdisturb: "<:donotdisturb:752564908956057692>",
+  experience: "<:experience:754238145951825951>",
+  idle: "<:idle:752564909169967214>",
+  offline: "<:offline:752564909279150151>",
+  online: "<:online:752564909140869141>",
+  streaming: "<:streaming:752564909358973028>"
 };
 
 
