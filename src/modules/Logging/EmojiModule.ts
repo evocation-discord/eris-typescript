a6a8d3fae29feea7f2b7342908ca8d0dbe3c522d
Stijn <stijn@kolkies.dev>
2020-09-27 19:40:57 +0200

    uwu

--- src/modules/Logging/EmojiModule.ts
+++ src/modules/Logging/EmojiModule.ts
@@ -17,6 +17,7 @@ export default class EmojiModule extends Module {
   @monitor({ event: "emojiUpdate" })
   async emojiUpdate(oldEmoji: GuildEmoji, newEmoji: GuildEmoji): Promise<Message> {
     if (newEmoji.guild.id !== MAIN_GUILD_ID) return;
+    if (newEmoji.name === oldEmoji.name) return;
     const channel = await this.client.channels.fetch(CHANNELS.PERIPHERAL_ANNOUNCEMENTS) as TextChannel;
     if (newEmoji.animated) return channel.send(strings.modules.emojis.animatedEmojiUpdated(oldEmoji, newEmoji));
     return channel.send(strings.modules.emojis.emojiUpdated(oldEmoji, newEmoji));
