09485551ca9b72721256b5ebfca594434fba7b24
Strects <70445685+Strects@users.noreply.github.com>
2020-10-03 11:51:59 +0100

    Update index.ts

--- src/lib/utils/messages/index.ts
+++ src/lib/utils/messages/index.ts
@@ -194,7 +194,7 @@ export const strings = {
       aboutCommand: [
         "Hi! I am a custom bot designed for exclusive use by Evocation staff and members. An impermeable forcefield that surrounds the universe of Evocation prohibits me from being able to join and interact with other servers.\n",
         "__**PRIMARY CONTRIBUTORS**__\n",
-        "**DEVELOPMENT TEAM LEAD**: <@209609796704403456>", // Stijn
+        "**DEVELOPMENT TEAM LEAD**: <@209609796704403456>\n", // Stijn
         "__**SECONDARY CONTRIBUTORS**__\n",
         "**CHARACTER CONCEPTUALIST**: <@369497100834308106>", // Ace
         "**ABSTRACTION CONFIDANT**: <@700418258687230083>" // yesfromskribbl
