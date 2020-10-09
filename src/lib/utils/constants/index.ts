5312a4665bc682c902565d64f255bc74a33e8b66
Stijn <stijn@kolkies.dev>
2020-10-09 22:59:48 +0200

    Add new regex url

--- src/lib/utils/constants/index.ts
+++ src/lib/utils/constants/index.ts
@@ -4,7 +4,7 @@ export * from "./env";
 
 // eslint-disable-next-line no-useless-escape
 export const linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
-export const messageLinkRegex = /^(?:https?):\/\/(?:(?:(?:canary|ptb)\.)?(?:discord|discordapp)\.com\/channels\/)(@me|\d+)\/(\d+)\/(\d+)$/g;
+export const messageLinkRegex = /(?:http(?:s)?)?:\/\/(?:(?:ptb|canary|development)\.)?discord(?:app)?\.com\/channels\/(\d{15,21})\/(\d{15,21})\/(\d{15,21})\/?/;
 
 export enum CommandCategories {
   "Bot Owner" = "Bot Owner",
