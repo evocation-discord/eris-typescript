79bbbbcebd295d53e8074ecd582a91d5ca53e984
Stijn <stijn@kolkies.dev>
2020-10-13 14:26:20 +0200

    refactor(staff): Affiliate and Moderation module

--- src/modules/Staff/ModerationModule.ts
+++ src/modules/Staff/ModerationModule.ts
@@ -1,18 +1,23 @@
-import { command, CommandCategories, commandDescriptions, Embed, errorMessage, inhibitors, messageLinkRegex, Module, strings } from "@lib/utils";
-import { Message, TextChannel } from "discord.js";
+import inhibitors from "@utils/inhibitors";
+import { commandDescriptions, errorMessage, strings } from "@utils/messages";
+import { command, CommandCategories } from "@utils/commands";
+import Discord from "discord.js";
+import { Module } from "@utils/modules";
+import { regex } from "@utils/constants";
+import Embed from "@utils/embed";
 
 export default class ModerationModule extends Module {
 
   @command({ inhibitors: [inhibitors.moderatorOnly], group: CommandCategories["Moderation"], args: [String], staff: true, usage: "<messageLink:string>", description: commandDescriptions.quote })
-  async quote(msg: Message, messageLink: string): Promise<void | Message> {
+  async quote(msg: Discord.Message, messageLink: string): Promise<void> {
     msg.delete();
-    const executedRegex = messageLinkRegex.exec(messageLink);
+    const executedRegex = regex.messageLinkRegex.exec(messageLink);
     if (!executedRegex) return errorMessage(msg, strings.general.error(strings.modules.util.linkDoesNotMatchDiscordLink));
     const [, guildId, channelId, messageId] = executedRegex;
     try {
       const guild = this.client.guilds.resolve(guildId);
       if (!guild) return errorMessage(msg, strings.general.error(strings.modules.util.guildWasNotFound(guildId)));
-      const channel = guild.channels.resolve(channelId) as TextChannel;
+      const channel = guild.channels.resolve(channelId) as Discord.TextChannel;
       if (!channel) return errorMessage(msg, strings.general.error(strings.modules.util.channelWasNotFound(channelId)));
       if (["Staff", "Oversight & Development", "Moderator Inbox", "Eris Registers"].includes(channel.parent.name)) return errorMessage(msg, strings.general.error(strings.modules.moderation.quote.restrictedChannel));
       if (["729429041022500885", "528598741565833246"].includes(channel.id)) return errorMessage(msg, strings.general.error(strings.modules.moderation.quote.restrictedChannel));
