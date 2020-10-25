0da080c71c44d3058d31ac05c4ad47595b0ad78e
Stijn <stijn@kolkies.dev>
2020-10-25 20:59:24 +0100

    fix(soulstone): fix database model

--- src/utils/database/models/SoulstoneShopItem.ts
+++ src/utils/database/models/SoulstoneShopItem.ts
@@ -1,10 +1,10 @@
 import {
-  Entity, BaseEntity, Column, PrimaryColumn
+  Entity, BaseEntity, Column, PrimaryGeneratedColumn
 } from "typeorm";
 
 @Entity("soulstoneshopitem")
 export class SoulstoneShopItem extends BaseEntity {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: string;
 
   @Column()
