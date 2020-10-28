5726c65a4887cb5760110225346d04acfab39a80
Stijn <stijn@kolkies.dev>
2020-10-28 15:34:15 +0100

    fix: update version text

--- src/utils/messages/general.ts
+++ src/utils/messages/general.ts
@@ -6,7 +6,7 @@ import Discord from "discord.js";
 
 export default {
   success: (text: string) => `${emotes.commandresponses.success} **SUCCESS**: ${text}`,
-  version: `${emotes.commandresponses.version} **VERSION**: Eris is currently running on version **1.5**, titled **THE EXPERIENCE UPDATE**, deployed on **12**/**09**/**2020** (DD/MM/YYYY).`,
+  version: `${emotes.commandresponses.version} **VERSION**: Eris is currently running on version **2**, titled **THE SOULSTONE UPDATE**, deployed on **12**/**09**/**2020** (DD/MM/YYYY).`,
   dmsclosed: "Your User Settings are preventing me from being able to send you Direct Messages. Please rectify this issue and try again.",
   checkdms: "Check your Direct Messages.",
   codeblockMember: (added: Discord.GuildMember[], removed: Discord.GuildMember[] = []): string => [
